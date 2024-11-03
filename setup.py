df3709ea370aaaaf92941f30f2b7b65dbcbef1b5
Joshua Vandaële <joshua@vandaele.software>
2024-11-03 16:29:52 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="sisou",  # Required
-    version="1.1.7",  # Required
+    version="1.1.8",  # Required
     description="A powerful tool to conveniently update all of your ISO files!",  # Optional
     long_description=long_description,  # Optional
     long_description_content_type="text/markdown",  # Optional
